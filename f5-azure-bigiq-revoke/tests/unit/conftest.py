d1d499c11368f0ec22a41d2e4ea7b666b5f16719
Greg Crosby <g.crosby@f5.com>
2020-08-03 10:03:21 -0700

    Esecldtplt 2009

---
+++ f5-azure-bigiq-revoke/tests/unit/conftest.py
@@ -0,0 +1,64 @@
+""" Test fixtures """
+
+import pytest
+
+
+@pytest.fixture
+def mock_bigiq_pool(monkeypatch):
+    """Set: BIGIQ license pool"""
+    monkeypatch.setenv("BIGIQ_LICENSE_POOL", "test_pool")
+
+
+@pytest.fixture
+def mock_bigiq_utility(monkeypatch):
+    """Set: BIGIQ utility key and offer"""
+    monkeypatch.setenv("BIGIQ_UTILITY_KEY", "test_utility_key")
+    monkeypatch.setenv("BIGIQ_UTILITY_OFFER", "test_utility_offer")
+
+
+@pytest.fixture
+def mock_bigiq_all(monkeypatch):
+    """Set: BIGIQ license pool, utility key and offer"""
+    monkeypatch.setenv("BIGIQ_LICENSE_POOL", "test_pool")
+    monkeypatch.setenv("BIGIQ_UTILITY_KEY", "test_utility_key")
+    monkeypatch.setenv("BIGIQ_UTILITY_OFFER", "test_utility_offer")
+
+
+@pytest.fixture
+def mock_bigiq_all_missing(monkeypatch):
+    """Remove: BIGIQ license pool, utility key and offer"""
+    monkeypatch.delenv("BIGIQ_LICENSE_POOL", raising=False)
+    monkeypatch.delenv("BIGIQ_UTILITY_KEY", raising=False)
+    monkeypatch.delenv("BIGIQ_UTILITY_OFFER", raising=False)
+
+
+@pytest.fixture
+def mock_main_env_variables(monkeypatch):
+    """Set: Azure and BIGIQ env vars"""
+    monkeypatch.setenv("AZURE_RESOURCE_GROUP", "my_group")
+    monkeypatch.setenv("AZURE_VMSS_NAME", "my_vmss_name")
+    monkeypatch.setenv("BIGIQ_ADDRESS", "10.1.1.1")
+    monkeypatch.setenv("BIGIQ_USERNAME", "admin")
+    monkeypatch.setenv("BIGIQ_PASSWORD", "some_password")
+
+
+@pytest.fixture
+def mock_main_env_variables_missing(monkeypatch):
+    """Remove: Azure and BIGIQ env vars"""
+    monkeypatch.delenv("AZURE_RESOURCE_GROUP", raising=False)
+    monkeypatch.delenv("AZURE_VMSS_NAME", raising=False)
+    monkeypatch.delenv("BIGIQ_ADDRESS", raising=False)
+    monkeypatch.delenv("BIGIQ_USERNAME", raising=False)
+    monkeypatch.delenv("BIGIQ_PASSWORD", raising=False)
+
+
+@pytest.fixture
+def mock_tenant_variables(monkeypatch):
+    """Set: TENANT var"""
+    monkeypatch.setenv("TENANT", "my_tenant")
+
+
+@pytest.fixture
+def mock_tenant_variables_missing(monkeypatch):
+    """Remove: TENANT var"""
+    monkeypatch.delenv("TENANT", raising=False)
---
+++ f5-azure-bigiq-revoke/timer_trigger_revoke/__init__.py
@@ -0,0 +1,177 @@
+"""Azure BIGIQ license pool revocation script"""
+# pylint: disable=no-name-in-module
+# pylint: disable=unused-argument
+# pylint: disable=line-too-long
+# pylint: disable=too-many-locals
+
+import os
+import logging
+import datetime
+
+from msrestazure.azure_active_directory import MSIAuthentication
+
+import azure.functions as func
+from azure.mgmt.resource import SubscriptionClient, ResourceManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+
+from f5sdk.bigiq import ManagementClient
+from f5sdk.bigiq.licensing import AssignmentClient
+from f5sdk.bigiq.licensing.pools import MemberManagementClient, UtilityOfferingMembersClient
+
+
+def get_pool_lic_type():
+    """Return BIGIQ license pool type"""
+    if "BIGIQ_LICENSE_POOL" in os.environ and \
+        "BIGIQ_UTILITY_KEY" not in os.environ and \
+            "BIGIQ_UTILITY_OFFER" not in os.environ:
+        licensing_mode = 'pool'
+    elif "BIGIQ_LICENSE_POOL" not in os.environ and \
+        "BIGIQ_UTILITY_KEY" in os.environ and \
+            "BIGIQ_UTILITY_OFFER" in os.environ:
+        licensing_mode = 'utility'
+    else:
+        raise Exception('You must provide either a registration key pool name or a utility license key and offer name, but not both!')
+    return licensing_mode
+
+
+def get_vmss_instances(group, resource, compute_client, resource_client):
+    """Get a list of all instances in specified vm scale set"""
+    provisioned = []
+    vmss = compute_client.virtual_machine_scale_set_vms.list(group, resource)
+    for instance in vmss:
+        instance_name = instance.name
+        instance_id = instance.instanceId
+        vmss_vm = compute_client.virtual_machine_scale_set_vms.get(group, resource, instance_id)
+        vmss_vm_provisioning_state = vmss_vm.provisioning_state
+        logging.info('Provisioning state for instance ' + instance_name + ' is: ' + vmss_vm_provisioning_state)
+        if vmss_vm_provisioning_state in ['Succeeded', 'Updating', 'Failed']:
+            nic = resource_client.resources.get_by_id(instance.network_profile.network_interfaces[0].id, api_version='2017-12-01')
+            ip_reference = nic.properties['ipConfigurations'][0]['properties']
+            private_ip = ip_reference['privateIPAddress']
+            mac_address = nic.properties['macAddress']
+        elif vmss_vm_provisioning_state in ['Creating']:
+            private_ip = 'creating'
+            mac_address = 'creating'
+        else:
+            private_ip = 'deleting'
+            mac_address = 'deleting'
+        provisioned.append({
+            'instance_name': instance_name,
+            'instance_id': instance_id,
+            'provisioning_state': vmss_vm_provisioning_state,
+            'private_ip': private_ip,
+            'mac_address': mac_address.replace("-", ":")})
+    logging.info('Instance dictionary: %s', str(provisioned))
+    return provisioned
+
+
+def get_bigiq_assignments(mgmt_client):
+    """Get all BIG-IQ license assignments"""
+    # create assignment client
+    assignment_client = AssignmentClient(mgmt_client)
+    assignments = assignment_client.list()
+    assignments = assignments['items']
+    if not assignments:
+        logging.info('Unable to locate any BIG-IQ assignments!')
+    return assignments
+
+
+def filter_tenant_assignments(assignments):
+    """Keep only the license assignments for our tenant"""
+    licensed = []
+    if os.getenv("TENANT") is None:
+        raise KeyError('TENANT is undefined and is required')
+    for assignment in assignments:
+        if assignment['tenant'] and assignment['tenant'] in os.environ['TENANT']:
+            licensed.append({
+                'private_ip': assignment['deviceAddress'],
+                'mac_address': assignment['macAddress'],
+                'id': assignment['id'],
+                'tenant': assignment['tenant']})
+    if not licensed:
+        logging.info('Unable to locate any licensed devices for this tenant!')
+    else:
+        logging.info('Assignment dictionary: %s', str(licensed))
+    return licensed
+
+
+def filter_provisioning_state(tenant_filtered, provisioned):
+    """Remove licenses from revoke list when provisioning state: Creating, Succeeded, or Updating"""
+    for licensed_device in tenant_filtered[:]:
+        for provisioned_device in provisioned:
+            if licensed_device['mac_address'] == provisioned_device['mac_address'] and \
+                    provisioned_device['provisioning_state'] in ['Creating', 'Succeeded', 'Updating']:
+                tenant_filtered.remove(licensed_device)
+    if not tenant_filtered:
+        logging.info('No licenses are eligible for revocation!')
+    else:
+        logging.info('Revocation dictionary: %s', str(tenant_filtered))
+    return tenant_filtered
+
+
+def revoke(licensing_mode, state_filtered, mgmt_client):
+    """Revoke specified licenses on BIGIQ"""
+    if licensing_mode == 'pool':
+        member_mgmt_client = MemberManagementClient(mgmt_client)
+        for unlicensed_device in state_filtered:
+            member_mgmt_client.create(
+                config={
+                    'licensePoolName': os.environ['BIGIQ_LICENSE_POOL'],
+                    'command': 'revoke',
+                    'address': unlicensed_device['private_ip'],
+                    'assignmentType': 'UNREACHABLE',
+                    'macAddress': unlicensed_device['mac_address'],
+                    'hypervisor': 'azure'
+                }
+            )
+    elif licensing_mode == 'utility':
+        utility_members_client = UtilityOfferingMembersClient(
+            mgmt_client,
+            pool_name=os.environ['BIGIQ_UTILITY_KEY'],
+            offering_name=os.environ['BIGIQ_UTILITY_OFFER']
+        )
+        for unlicensed_device in state_filtered:
+            utility_members_client.delete(
+                name=unlicensed_device['id'],
+                config={
+                    'assignmentType': 'UNREACHABLE',
+                    'macAddress': unlicensed_device['mac_address'],
+                    'hypervisor': 'azure'
+                }
+            )
+    else:
+        raise Exception('Invalid BIG-IQ info specified, check app settings!')
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    """Discover vms and revoke if necessary unused licenses on BIGIQ"""
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    # Create MSI authentication - requires a system managed identity assigned to this function
+    credentials = MSIAuthentication()
+
+    # Create Azure subscription client
+    subscription_client = SubscriptionClient(credentials)
+    subscription = next(subscription_client.subscriptions.list())
+    subscription_id = subscription.subscription_id
+
+    # Create Azure management clients
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    compute_client = ComputeManagementClient(credentials, subscription_id)
+
+    # Set variables
+    group = os.environ['AZURE_RESOURCE_GROUP']
+    resource = os.environ['AZURE_VMSS_NAME']
+    mgmt_client = ManagementClient(
+        os.environ['BIGIQ_ADDRESS'],
+        user=os.environ['BIGIQ_USERNAME'],
+        password=os.environ['BIGIQ_PASSWORD'])
+    licensing_mode = get_pool_lic_type()
+    provisioned = get_vmss_instances(group, resource, compute_client, resource_client)
+    assignments = get_bigiq_assignments(mgmt_client)
+    tenant_filtered = filter_tenant_assignments(assignments)
+    state_filtered = filter_provisioning_state(tenant_filtered, provisioned)
+    # Revoke
+    revoke(licensing_mode, state_filtered, mgmt_client)
+    logging.info('Finished license revocation.')
